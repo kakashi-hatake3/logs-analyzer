8e0b9b62bba0dca3cd77be7490a0720f8abb3d7b
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-10-18 01:58:46 +0300

    test: написал тест для format_handler

---
+++ tests/test_format_handler.py
@@ -0,0 +1,17 @@
+import pytest
+
+from src.format_handler import FormatHandler
+from src.wrong_input_error import WrongParameterNameError
+
+
+@pytest.mark.parametrize(
+    "params, expected",
+    [
+    ({'path': 'scnd', 'format': 'adoc'}, 'adoc'),
+    ({'path': 'scnd', 'format': 'aboba'}, WrongParameterNameError('aboba')),
+    ({'path': 'scnd', 'from': 'ss'}, None),
+    ]
+                         )
+def test_format_handler(params, expected):
+    format_handler = FormatHandler(params)
+    assert format_handler.get_format() == expected
