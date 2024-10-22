1ccd2659c3f5d43a3cb1bda97be5ee6cf4f568a1
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-10-22 19:58:33 +0300

    refactor: добавил enum для параметров лога и для параметров ввода

--- src/enums.py
+++ src/enums.py
@@ -9,3 +9,26 @@ class Formats(StrEnum):
 class PathTypes(StrEnum):
     url = "url"
     local = "local"
+
+
+class InputParameters(StrEnum):
+    path = "path"
+    from_date = "from"
+    to_date = "to"
+    file_format = "format"
+    filter_field = "filter-field"
+    filter_value = "filter-value"
+
+
+class LogFields(StrEnum):
+    ip = "ip"
+    client_id = "client_id"
+    user_id = "user_id"
+    time_local = "time_local"
+    method = "method"
+    path = "path"
+    protocol = "protocol"
+    status_code = "status_code"
+    response_size = "response_size"
+    referrer = "referrer"
+    agent = "agent"
