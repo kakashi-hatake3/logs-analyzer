d5db70c42ee56b64a13ed64fb41e49b7242e2d46
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-10-22 19:57:06 +0300

    test: написал тесты для обработки лога

---
+++ tests/test_logs_handler.py
@@ -0,0 +1,16 @@
+import pytest
+
+from src.logs_handler import LogsHandler
+from src.path_handler import PathHandler
+
+
+@pytest.mark.parametrize("params", (
+        {'from': 'ss', 'path': 'aboba'},
+        {'from': 'ss'},
+        {'path': 'https://vk.com/alsldla'},
+)
+                         )
+def test_logs_handler_wrong_input(params):
+    path_handler = PathHandler(params)
+    with pytest.raises(SystemExit):
+        LogsHandler(path_handler).get_logs()
