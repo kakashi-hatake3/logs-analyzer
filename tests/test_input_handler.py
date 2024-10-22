6dabab06a17dc7b93e8e3a924fcb459c6fb533ae
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-10-22 20:04:29 +0300

    test: разбил тесты на позитивные и негативные

--- tests/test_input_handler.py
+++ tests/test_input_handler.py
@@ -7,13 +7,19 @@ from src.wrong_input_error import WrongInputError, WrongParameterNameError, Wron
 @pytest.mark.parametrize(
     "params, expected",
     [
-        (['main.py', '--path', 'scnd', '--from', 'ss'], {'path': 'scnd', 'from': 'ss'}),
         (['main.py', '-path', 'scnd', '--from', 'ss'], WrongInputError()),
         (['main.py', '--aboba', 'scnd', '--from', 'ss'], WrongParameterNameError('aboba')),
         (['main.py', '--aboba'], WrongCountParametersError(2)),
     ]
                          )
-def test_input_mapper(params, expected):
+def test_input_mapper_negative(params, expected):
     mapper = InputMapper()
     mapper.parameters = params
-    assert mapper.get_mapped_params() == expected
+    with pytest.raises(SystemExit):
+        assert mapper.get_mapped_params() == expected
+
+
+def test_input_mapper_positive():
+    mapper = InputMapper()
+    mapper.parameters = ['main.py', '--path', 'scnd', '--from', 'ss']
+    assert mapper.get_mapped_params() == {'path': 'scnd', 'from': 'ss'}
