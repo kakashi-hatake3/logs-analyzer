7767363278fe92d161e5f45e06d7a5e9334fa40d
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-10-22 19:59:27 +0300

    refactor: разделил функциональность одного класса на два

--- src/input_handler.py
+++ src/input_handler.py
@@ -1,8 +1,8 @@
 from sys import argv
 from typing import List, Dict
 
-from src.wrong_input_error import WrongCountParametersError
-from src.wrong_input_error import WrongInputError, WrongParameterNameError
+from src.enums import InputParameters
+from src.wrong_input_error import WrongInputError, WrongParameterNameError, WrongCountParametersError
 
 
 class InputHandler:
@@ -18,42 +18,62 @@ class InputMapper:
 
     def __init__(self):
         self.parameters = InputHandler().values
-        self._command_count = 0
+        self.command_count = 0
         self._mapped_params: Dict[str, str] = {}
 
     def _map_input(self) -> None:
         """Заполняем словарь."""
+        define_command_parameter_str = "--"
         is_should_add = False
         should_add_param = ""
         for parameter in self.parameters:
             if is_should_add:
                 self._mapped_params[should_add_param] = parameter
                 is_should_add = False
-            if parameter[:2] == "--":
-                self._command_count += 1
+            if parameter[:2] == define_command_parameter_str:
+                self.command_count += 1
                 is_should_add = True
                 should_add_param = parameter[2:]
-
-    def _check_is_correct(self) -> None:
-        """Проверяем на ошибку."""
-        if len(self.parameters) < 3:
-            raise WrongCountParametersError(len(self.parameters))
-        if self._command_count * 2 + 1 != len(self.parameters):
-            raise WrongInputError
-        for key in self._mapped_params.keys():
-            if key not in ["path", "from", "to", "format", "filter-field", "filter-value"]:
-                raise WrongParameterNameError(key)
 
     def get_mapped_params(self) -> Dict[str, str] | Exception:
         """Возвращаем словарь или ошибку."""
         try:
             self._map_input()
-            self._check_is_correct()
+            CheckParameters(self.parameters, self.command_count, self._mapped_params).check_correctness()
         except WrongCountParametersError as e:
-            return e
+            print(e)
+            exit()
         except WrongInputError as e:
-            return e
+            print(e)
+            exit()
         except WrongParameterNameError as e:
-            return e
+            print(e)
+            exit()
         else:
             return self._mapped_params
+
+
+class CheckParameters:
+    """Проверка параметров."""
+
+    def __init__(self, parameters, command_count, mapped_params):
+        self.mapped_params = mapped_params
+        self.command_count = command_count
+        self.parameters = parameters
+
+    def check_correctness(self) -> None:
+        """Проверяем на ошибку."""
+        if len(self.parameters) < 3:
+            raise WrongCountParametersError(len(self.parameters))
+        if self.command_count * 2 + 1 != len(self.parameters):
+            raise WrongInputError
+        for key in self.mapped_params.keys():
+            if key not in [
+                InputParameters.path,
+                InputParameters.from_date,
+                InputParameters.to_date,
+                InputParameters.file_format,
+                InputParameters.filter_field,
+                InputParameters.filter_value,
+            ]:
+                raise WrongParameterNameError(key)
